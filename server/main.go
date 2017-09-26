d15247bf3b4896a916444eb14aeb44e399a564bf
Raj Kiran Gade <rajkiran.gade@gmail.com>
2017-09-26 18:02:44 +0530

    protofile complete

---
+++ server/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"google.golang.org/grpc"
+	"golang.org/x/net/context"
+	pb "../../protofiles/"	
+	"google.golang.org/grpc/reflection"
+	"github.com/golang/protobuf"
+	
+)
+
+const (
+	port = ":50051"
+)
+
+
+func main() {
+
+
+}
